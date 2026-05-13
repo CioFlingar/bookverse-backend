141a43fbd0cd81c13f7d47c4c7cc6a072fc27f6c
CioF <coder.walidhasan@gmail.com>
2026-05-13 14:21:44 +0600

    update user model

--- src/models/User.js
+++ src/models/User.js
@@ -8,6 +8,8 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: "reader" }, // 'reader' or 'admin'
+    rememberMeToken: { type: String },
+    rememberMeExpiry: { type: Date },
   },
   { timestamps: true },
 );
