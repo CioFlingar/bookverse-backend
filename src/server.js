3551373a01a24b5fa89b74cbfdc2f0b384c66725
CioF <coder.walidhasan@gmail.com>
2026-05-13 14:24:48 +0600

    link changes in server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,11 @@ import connectDB from "./config/db.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import libraryRoutes from "./routes/libraryRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import quoteRoutes from "./routes/quoteRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
@@ -18,12 +23,14 @@ app.use(cors());
 
 // Routes
 app.use("/api/books", bookRoutes);
-
 app.use("/api/auth", authRoutes);
-
 app.use("/api/users", userRoutes);
-
 app.use("/api/admin", adminRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/library", libraryRoutes);
+app.use("/api/quotes", quoteRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
